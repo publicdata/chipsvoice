117dee812cc7c9a316d51cd6b4477c5acd57c855
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-10-21 11:23:48 -0600

    get synth working

--- googleTextToSpeech1/app.js
+++ googleTextToSpeech1/app.js
@@ -4,6 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Imports Google Cloud client library for TTS
+const textToSpeech = require('@google-cloud/text-to-speech');
+// Import other required libraries
+const fs = require('fs');
+const util = require('util');
+// Creates a client
+const client = new textToSpeech.TextToSpeechClient();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -31,6 +38,7 @@ app.post("/hook", (req, res) => {
 
 app.post("/synth", (req, res) => {
   console.log(req.body) // Call your action on the request here
+  quickStart();
   res.status(200).end() // Responding is important
 })
 
@@ -52,6 +60,25 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+async function quickStart() {
+  // The text to synthesize
+  const text = 'hello, world!';
 
+  // Construct the request
+  const request = {
+    input: {text: text},
+    // Select the language and SSML voice gender (optional)
+    voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
+    // select the type of audio encoding
+    audioConfig: {audioEncoding: 'MP3'},
+  };
+
+  // Performs the text-to-speech request
+  const [response] = await client.synthesizeSpeech(request);
+  // Write the binary audio content to a local file
+  const writeFile = util.promisify(fs.writeFile);
+  await writeFile('output.mp3', response.audioContent, 'binary');
+  console.log('Audio content written to file: output.mp3');
+}
 
 module.exports = app;
