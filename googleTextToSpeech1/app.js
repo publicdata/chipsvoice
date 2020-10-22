e3322dbc8ba569c07a09897c55623a2a32e8b499
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-10-21 20:51:28 -0600

    setup cors

--- googleTextToSpeech1/app.js
+++ googleTextToSpeech1/app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var cors = require('cors')
 var logger = require('morgan');
 const { spawn, exec } = require('child_process');
 
@@ -16,12 +17,13 @@ const client = new textToSpeech.TextToSpeechClient();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+var app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
