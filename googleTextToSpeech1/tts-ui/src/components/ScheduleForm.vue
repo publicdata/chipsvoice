b908854e3e9774d3c26b79cd68349057a3db95cb
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-10-26 17:35:19 -0600

    start a scheduler from

---
+++ googleTextToSpeech1/tts-ui/src/components/ScheduleForm.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  v-container
+    v-row
+      v-col
+        v-card
+          v-card-title Schedule Clip
+          v-card-text            
+            v-switch(v-model="repeat" label="Repeat")
+            v-slider(label="Count" min="0" max="10" v-model="count" thumb-label)
+            v-slider(label="Interval" min="0" max="3600" step="15" v-model="interval" thumb-label)
+          v-card-actions
+            v-spacer
+            v-btn(@click="save") Save
+            v-btn(@click="cancel") Cancel
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: 'ScheduleForm',
+
+    data: () => ({
+      repeat: true,
+      count: 0,
+      interval: 3,
+    }),
+
+    methods: {
+      save() {},
+      cancel() {},
+    }
+  })
+</script>
+
+<style lang="sass" scoped>
+.voice-options, .actions
+  display: flex
+</style>
